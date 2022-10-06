4fc0f69c159f55b68c0b6b76c43d19887c2d8078
Santiago Pintus <santiagopintus@gmail.com>
2022-10-06 20:30:29 -0300

    Added config options: Fontsize, text align, and print btn

--- js/config.js
+++ js/config.js
@@ -1,5 +1,43 @@
-const alignOptions = { left: "left", center: "center", right: "right" };
 const fonts = {
   times: "'Times New Roman', Times, serif",
   arial: "Arial, Helvetica, sans-serif",
 };
+
+const main = () => {
+  const $tags = document.querySelectorAll(".tag");
+  listenAlignment($tags);
+  listenFontSize($tags)
+  listenPrint();
+};
+
+const listenFontSize = $tags => {
+  let inp = document.getElementById("fontSizeInp");
+  inp.addEventListener('change', (e) => {
+    $tags.forEach(tag => {
+      tag.style.fontSize = `${e.target.value}px`;
+    })
+  })
+}
+
+const listenAlignment = $tags => {
+  let leftBtn = document.getElementById("alignLeft");
+  let centerBtn = document.getElementById("alignCenter");
+  let rightBtn = document.getElementById("alignRight");
+  leftBtn.addEventListener('click', () => updateAlignment($tags, "start"));
+  centerBtn.addEventListener('click', () => updateAlignment($tags, "center"));
+  rightBtn.addEventListener('click', () => updateAlignment($tags, "end"));
+};
+
+const updateAlignment = ($tags, option) => {
+  $tags.forEach(tag => {
+    tag.style.justifyContent = option;
+  });
+};
+
+const listenPrint = () => {
+  document.getElementById("printBtn").addEventListener("click", () => {
+    window.print();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', main);
